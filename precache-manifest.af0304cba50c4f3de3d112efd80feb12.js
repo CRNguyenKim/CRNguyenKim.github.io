887a57f95272ecedd1f950f5f85d03ed38a509cf
nguyenkim <crnguyenkim@gmail.com>
2019-06-25 19:24:18 +0700

    Updates

---
+++ precache-manifest.af0304cba50c4f3de3d112efd80feb12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "449270b9f521e192bd7f8bc1f00d1f65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac0b99a74087adb23c1b",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "4d7d3e2061e8b2c6991c",
+    "url": "/static/css/main.5bc7b728.chunk.css"
+  },
+  {
+    "revision": "ac0b99a74087adb23c1b",
+    "url": "/static/js/2.f5f73f44.chunk.js"
+  },
+  {
+    "revision": "4d7d3e2061e8b2c6991c",
+    "url": "/static/js/main.52c47642.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "c709324c7dbfd9825b61202f4cefc09c",
+    "url": "/static/media/logo_nk_dark.c709324c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa2c638a67af5461bda4837398aeff48.js"
+  "/precache-manifest.af0304cba50c4f3de3d112efd80feb12.js"
 );
 
 self.addEventListener('message', (event) => {
