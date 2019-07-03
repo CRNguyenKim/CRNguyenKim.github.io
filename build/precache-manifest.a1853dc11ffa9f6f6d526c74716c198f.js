bf548bcaf0711680eb8458dd1cf76c6303dbc513
nguyenkim <crnguyenkim@gmail.com>
2019-07-03 13:29:43 +0700

    change fav icon, app title

--- build/precache-manifest.a1853dc11ffa9f6f6d526c74716c198f.js
+++ build/precache-manifest.a1853dc11ffa9f6f6d526c74716c198f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5e81b8acf651a7d206961b70c81050a2",
+    "revision": "320e43ae57ddfca4e83b6ff90bce7250",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "6bc29eeb9ec34ab66c7d",
+    "revision": "22eca18632a9fbf67108",
     "url": "/static/css/main.5bc7b728.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.956a96f9.chunk.js"
   },
   {
-    "revision": "6bc29eeb9ec34ab66c7d",
-    "url": "/static/js/main.b1245434.chunk.js"
+    "revision": "22eca18632a9fbf67108",
+    "url": "/static/js/main.30ba28fb.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
