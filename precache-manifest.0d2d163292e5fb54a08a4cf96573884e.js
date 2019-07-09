5a07f8c0a2ec8484c3a8d06d83a2c5f50011a1ff
nguyenkim <crnguyenkim@gmail.com>
2019-07-09 10:30:46 +0700

    Updates

---
+++ precache-manifest.0d2d163292e5fb54a08a4cf96573884e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6acee4c2d41cb0337ab4a84a8a9dbc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "165dfca4be392c195ec8",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "bed87d1f0d60c8825192",
+    "url": "/static/css/main.5bc7b728.chunk.css"
+  },
+  {
+    "revision": "165dfca4be392c195ec8",
+    "url": "/static/js/2.9d446259.chunk.js"
+  },
+  {
+    "revision": "bed87d1f0d60c8825192",
+    "url": "/static/js/main.56433a3b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
